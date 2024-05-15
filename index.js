62f0f99e5f2fec942eed227d31f5d023e1e8b768
ashoktechai <ashoktechai@gmail.com>
2024-05-15 17:12:20 +0530

    addpath

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const bodyParser = require("body-parser");
 const firmRoutes = require("./routes/firmRoutes");
 const productRoutes = require("./routes/productRoutes");
 const path = require("path");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 
